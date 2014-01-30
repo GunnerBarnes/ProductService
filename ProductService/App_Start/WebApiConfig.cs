df1cb79ae086e32e4ec5991834bd692b4c74206c
gbarnes <gbarnes@gavant.com>
2014-01-29 19:38:44 -0500

    Initial commit

---
+++ ProductService/App_Start/WebApiConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.OData.Builder;
+using ProductService.Models;
+
+namespace ProductService
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            ODataConventionModelBuilder builder = new ODataConventionModelBuilder();
+            builder.EntitySet<Product>("Products");
+            config.Routes.MapODataRoute("odata", "odata", builder.GetEdmModel());
+        }
+    }
+}
